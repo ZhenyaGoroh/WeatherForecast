11e0a96c9b59ccc7ba86894f05fbb57e28b473b6
ZhenyaGoroh <zhenyagoroh@gmail.com>
2023-02-25 00:13:48 +0300

    citiesData

--- src/components/CitySelect/CitySelect.tsx
+++ src/components/CitySelect/CitySelect.tsx
@@ -1,52 +1,58 @@
 import React, { useEffect, useState } from "react";
 import s from "./CitySelect.module.scss";
-// import { cities } from 'cities.json';
 
-interface Country {
-  iso2: string;
-  iso3: string;
-  countryL: string;
-  cities: string[];
+interface CitiesData {
+  city: string;
+  country: string;
+  countryCode: string;
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+  population: number;
+  region: string;
+  regionCode: string;
+  type: string;
+  wikiDataId: string;
 }
 
 function CitySelect() {
   const [cityName, setCityName] = useState<string>("");
-  const [countries, setCountries] = useState<Country[]>();
-  const [cities, setCities] = useState<string[]>([]);
-
-  // const setCitiesFunc = (str: string) => {
-  //   const arr = [];
-  //   if (str.length > 3) {
-  //     for (const key in countries) {
-  //       countries[key].cities.map((el) =>
-  //         el.startsWith(str) ? arr.push(el) : null
-  //       );
-  //     }
-  //   }
-  //   console.log(arr);
-  // };
+  const [citiesData, setCitiesData] = useState<CitiesData[]>();
 
   useEffect(() => {
-    const fetchData = () => {
-      const API_KEY = import.meta.env.VITE_GEODB_CITIES_KEY;
-      const options = {
-        method: "GET",
-        headers: {
-          "X-RapidAPI-Key": API_KEY,
-          "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
-        },
-      };
-
-      fetch(
-        "https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=10&namePrefix=Babruysk",
-        options
-      )
-        .then((response) => response.json())
-        .then((response) => console.log(response))
-        .catch((err) => console.error(err));
+    const API_KEY = import.meta.env.VITE_GEODB_CITIES_KEY;
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": API_KEY,
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+      },
     };
-    fetchData();
-  }, []);
+    const getData = setTimeout(() => {
+      // console.log(cityName);
+      if (cityName.length > 3) {
+        fetch(
+          `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=10&minPopulation=10000&sort=-population&namePrefix=${cityName}`,
+          options
+        )
+          .then((response) => response.json())
+          .then((data) => setCitiesData(data.data))
+
+          .catch((err) => console.error(err));
+      } else {
+        setCitiesData([]);
+      }
+
+      // console.log(citiesData);
+    }, 2000);
+
+    return () => clearTimeout(getData);
+  }, [cityName]);
+
+  const citiesDiv = citiesData?.map((city) =>
+    city.type === "CITY" ? <div key={city.id}>{city.name}</div> : null
+  );
 
   return (
     <div className={s.citySelect}>
@@ -55,12 +61,13 @@ function CitySelect() {
         value={cityName}
         onChange={(e) => {
           setCityName(e.target.value);
-          // setCitiesFunc(cityName);
-          // console.log(cities);
+
+          // console.log(cityName);
         }}
         className={s.citySelect__input}
       />
-      {cityName.length > 3 && <div>{cityName}</div>}
+
+      <div>{citiesDiv}</div>
     </div>
   );
 }
